fd26272b078e1ac82bbb25067d10366e19ade60f
DrNox08 <marcopergamena.01@gmail.com>
2026-01-07 15:53:10 +0100

    Empty Project

---
+++ Assets/Scripts/DebugComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class DebugComponent : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("HELLO!");
+        }
+    }
+    
+    
+}
